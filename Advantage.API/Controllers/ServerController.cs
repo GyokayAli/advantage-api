90b4eaf68f1f73626c3534d454c220273f43df03
GyokayAli <gyokay.metin.ali@gmail.com>
2020-02-04 17:55:31 +0100

    Implement Server controller

---
+++ Advantage.API/Controllers/ServerController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Advantage.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Advantage.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServerController : ControllerBase
+    {
+        private readonly ApiContext _ctx;
+
+        public ServerController(ApiContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var response = _ctx.Server.OrderBy(s => s.Id).ToList();
+            return Ok(response);
+        }
+
+        [HttpGet("{id}", Name = "GetServer")]
+        public IActionResult Get(int id)
+        {
+            var response = _ctx.Server.Find(id);
+            return Ok(response);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Message(int id, [FromBody] ServerMessage msg)
+        {
+            var server = _ctx.Server.Find(id);
+            if (server == null)
+            {
+                return NotFound();
+            }
+
+            // Refactor: move into a service
+            if (msg.Payload == "activate")
+            {
+                server.IsOnline = true;
+            }
+
+            if (msg.Payload == "deactivate")
+            {
+                server.IsOnline = false;
+            }
+
+            _ctx.SaveChanges();
+            return new NoContentResult();
+        }
+    }
+}
